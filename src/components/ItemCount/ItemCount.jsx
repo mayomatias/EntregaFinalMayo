6b76689b8cb4492a5baf6d89e05b741fdb122013
Matias Mayo <mayomatiass@gmail.com>
2023-05-23 14:20:13 -0300

    Nuevo componente

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ItemCount = () => {
+    return (
+        <>
+            <div>ItemCount</div>
+            <div>
+                <button>+</button>
+                <p></p>
+                <button>-</button>
+            </div>
+        </>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import ItemCount from "../ItemCount/ItemCount";
 
 const ItemListContainer = ({greetting}) => {
     return(
-        <h2 style={{marginTop:'1rem', textAlign:'center'}}>{greetting}</h2>
+        <>
+            <h2 style={{marginTop:'1rem', textAlign:'center'}}>{greetting}</h2>
+            <ItemCount />
+        </>
     )
 }
 
