431eab9fe10cba82b86f6236bd9a109897a6753b
mayomatias <mayomatiass@gmail.com>
2023-06-08 19:19:02 -0300

    LinkACart

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 const CartWidget = () => {
   return(
     <div>
       <a href="">
+      <Link to={'/cart'}>
         <ShoppingCartIcon sx={{ color: '#ffffff' }} />
+      </Link>
+        
       </a>
     </div>
   )
