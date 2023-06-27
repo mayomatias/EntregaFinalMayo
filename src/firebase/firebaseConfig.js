bd62c66dae48dfbf3d3f563a57529d0c50cee890
Matias Mayo <mayomatiass@gmail.com>
2023-06-27 16:38:47 -0300

    Firebase conectado

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAZhHnKMSn3Wi405-GV2YsC0LjQxm_2f6k",
-    authDomain: "gammershopfinal.firebaseapp.com",
-    projectId: "gammershopfinal",
-    storageBucket: "gammershopfinal.appspot.com",
-    messagingSenderId: "769751899920",
-    appId: "1:769751899920:web:68a2918ceb045596b14f95"
+
   };
   
 
