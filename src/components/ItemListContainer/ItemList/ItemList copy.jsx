2e7656897d817ee5a49fcf524258732b70e596da
Matias Mayo <mayomatiass@gmail.com>
2023-06-30 13:32:39 -0300

    Conexion exitosa

--- src/components/ItemListContainer/ItemList/ItemList copy.jsx
+++ src/components/ItemListContainer/ItemList/ItemList copy.jsx
@@ -21,47 +21,7 @@ const ItemList = () => {
   const { data, loading } = useAxios('https://fakestoreapi.com/products/category/electronics');
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
   
-   /* const setProducts = async () => {
-    for (const product of datos) {
-      
-    }
-      datos.forEach(product => {
-      setDoc(doc(collection(db, "products")), {
-            title: product.title,
-            price: product.price,
-            description: product.description,
-            category: product.category,
-            image: product.image,
-            rating: {
-              rate: product.rate || 0,
-              count: product.count
-            }
-          })
-      });
-    }
-    console.log(doc(collection(db, "products")));
-
-    setProducts(); */
-    const fetchItemData = async () => {
-      try {
-        const q = query(collection(db, "products"));
-        const docs = [];
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-          // console.log('DATA:', doc.data(), 'ID:', doc.id);
-          docs.push({ ...doc.data(), id: doc.id });
-        });
-        setItems(docs)
-        console.log(items)
-      } catch (error) {
-        console.log("Error al obtener los datos de Firestore:", error);
-      }
-    };
-
-    fetchItemData();
-  }, []);
 
 
   if (loading) return <div className="center"> <CircularProgress /> </div>
--- src/components/ItemListContainer/ItemList/ItemList.jsx
+++ src/components/ItemListContainer/ItemList/ItemList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Item from "./Item/Item";
 import useAxios from '../../../hooks/useAxios';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -8,57 +8,33 @@ import { doc, collection, query, where, getDocs, getDoc } from "firebase/firesto
 import { db } from '../../../firebase/firebaseConfig';
 
 const ItemList = () => {
-  const { data, loading } = useAxios('https://fakestoreapi.com/products/category/electronics');
+ // const { data, loading } = useAxios('https://fakestoreapi.com/products/category/electronics');
+  const [todos, setTodos] = useState([])
+
+  const fetchPost = async () => {   
+    await getDocs(collection(db, "products"))
+        .then((querySnapshot)=>{              
+            const newData = querySnapshot.docs
+                .map((doc) => ({...doc.data(), id:doc.id }));
+            setTodos(newData);                
+            console.log(todos, newData);
+    })  
+  }
 
   useEffect(() => {
-    const verificarProductosCargados = async (productos) => {
-      for (const producto of productos) {
-        const querySnapshot = await getDocs(
-          query(collection(db, 'products'), where('title', '==', producto.title))
-        );
+  
+    fetchPost();
 
-        if (querySnapshot.empty) {
-          console.log('El producto no se creó en la base de datos:', producto.title);
-        } else {
-          console.log('El producto creado en la base de datos:', producto.title);
-        }
-      }
-    };
+  }, []);
 
-    const consultarProductoPorId = async (productId) => {
-      try {
-        const productRef = doc(db, 'products', productId);
-        const docSnapshot = await getDoc(productRef);
-
-        if (docSnapshot.exists()) {
-          const productData = docSnapshot.data();
-          console.log('Producto encontrado:');
-          console.log('ID:', docSnapshot.id);
-          console.log('Título:', productData.title);
-          console.log('Precio:', productData.price);
-          console.log('Descripción:', productData.description);
-        } else {
-          console.log('No se encontró ningún producto con el ID proporcionado.');
-        }
-      } catch (error) {
-        console.error('Error al consultar el producto:', error);
-      }
-    };
-
-    if (data) {
-      verificarProductosCargados(data);
-      consultarProductoPorId('J5deKjJvBFqmxDosdrDQ');
-    }
-  }, [data]);
-
-  if (loading) return <div className="center"> <CircularProgress /> </div>;
+  //if (loading) return <div className="center"> <CircularProgress /> </div>;
 
   return (
     <div className="items-list">
-      {data &&
-        data.map((data) => (
-          <div key={data.id}>
-            <Item item={data} />
+      {todos &&
+        todos.map((todos, i) => (
+          <div key={i}>
+            <Item item={todos} />
           </div>
         ))}
     </div>
